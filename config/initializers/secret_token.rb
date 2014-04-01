0c389ac7d84b94dddd54bd516ae26dc3d6a41aed
Michael Taylor <mjt145@gmail.com>
2014-04-01 22:02:02 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openadtools::Application.config.secret_key_base = '56775ee2b67a33a21bded705a677c718287161bbe49ae31812015d2bd6dd31adbfd99d2e4f293d9c4b11c18d680a1466e30da300821874009d378b4ae748a250'
