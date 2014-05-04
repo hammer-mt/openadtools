697b57b4865667d76edf17890966543f82d3dd54
Michael Taylor <mjt145@gmail.com>
2014-05-04 22:33:07 +0100

    cleaned up unbuilt tools

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,61 +2,16 @@ class ToolsController < ApplicationController
   def linktracker
   end
 
-  def socialstats
-  end
-
   def encoderdecoder
   end
 
-  def percentcalculator
-  end
-
   def absignificance
-  end
-
-  def descriptivestatistics
-  end
-
-  def bubblechart
-  end
-
-  def twitterlist
-  end
-
-  def runrate
-  end
-
-  def sharedcount
-  end
-
-  def graduatetest
-  end
-
-  def regexcheatsheet
-  end
-
-  def famousblogposts
-  end
-
-  def exceltips
-  end
-
-  def urlvalidator
-  end
-
-  def editorialcalendar
-  end
-
-  def treediagram
   end
 
   def tabtocsv
   end
 
   def followerfinder
-  end
-
-  def adsizes
   end
 
   def campaigncalculator
